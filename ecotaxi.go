ede8cbe29ead771f1454086d3213487b0625b576
omar.rodriguez.fernandez.contractor <omar.rodriguez.fernandez.contractor@bbva.com>
2023-01-18 17:47:07 -0400

    cambio

--- ecotaxi.go
+++ ecotaxi.go
@@ -25,5 +25,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
         if target == "" {
                 target = "World"
         }
-        fmt.Fprintf(w, "Hello %s!\n", target)
+        fmt.Fprintf(w, "***Hello %s!\n", target)
 }
